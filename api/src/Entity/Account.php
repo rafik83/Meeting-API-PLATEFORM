6fd5aed38df07c6eb351704bd4f7a0cc2d7318c4
Gregory Pelletier <gpelletier@ip512.com>
2021-01-04 09:17:50 +0100

    init symfony project

---
+++ api/src/Entity/Account.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\AccountRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=AccountRepository::class)
+ */
+class Account
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $vimeetId;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getVimeetId(): ?int
+    {
+        return $this->vimeetId;
+    }
+
+    public function setVimeetId(int $vimeetId): self
+    {
+        $this->vimeetId = $vimeetId;
+
+        return $this;
+    }
+}
