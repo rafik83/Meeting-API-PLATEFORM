9f6b3349a1a05195ddadedc0d7ac2a2db2031cfe
BertrandD <volubyl@pm.me>
2021-01-12 11:30:33 +0100

    Add admin section and remove blog to have an example of localized subsection

---
+++ client/src/routes/[locale([a-z]{2})]/admin/index.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { getTranslator } from '../../../modules/i18n';
+
+  const _ = getTranslator();
+</script>
+
+<svelte:head>
+  <title>Admin</title>
+</svelte:head>
+<h1>{$_('admin.title')}</h1>
