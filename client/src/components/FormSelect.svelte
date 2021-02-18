a2d0a5eb74021b34d42dabe9da95567bc7e3ef59
Antoine Smagghe <smagghe@proximumgroup.com>
2021-02-18 18:29:52 +0100

    feat 2330 - Create new form components

---
+++ client/src/components/FormSelect.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let options;
+  export let name;
+  export let optionsTitle;
+  export let id;
+  export let errorMessage = null;
+</script>
+
+<!-- svelte-ignore a11y-no-onchange -->
+<select
+  {name}
+  {id}
+  on:change
+  class="border bg-gray-50 rounded-3xl w-full mt-2 p-2 text-sm font-bold {errorMessage
+    ? 'border-error text-error'
+    : 'border-gray-200'}">
+  <option value={null}>{optionsTitle}</option>
+  {#each options as option}
+    <option value={option.id}>{option.name}</option>
+  {/each}
+</select>
---
+++ client/src/components/FormTextarea.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { _ } from 'svelte-i18n';
+
+  export let max = null;
+  export let value;
+  export let placeholder;
+  export let errorMessage = null;
+
+  $: if (max && value.length > max) {
+    value = value.substring(0, max);
+  }
+</script>
+
+<textarea
+  class="border bg-gray-50 rounded-xl w-full h-40 mt-2 italic p-2 text-sm {errorMessage
+    ? 'border-error text-error'
+    : 'border-gray-200'}"
+  bind:value
+  {placeholder}
+/>
+
+{#if max}
+  <p>
+    {$_('registration.characters', { values: { n: value.length } })}
+  </p>
+{/if}
