ae2b4ad6244284062409c2c9eb48f56665468a1b
emilieplz <plouzeau@proximumgroup.com>
2021-02-18 16:27:59 +0100

    2330 - Drag and drop

--- client/src/components/DragAndDrop.svelte
+++ client/src/components/DragAndDrop.svelte
@@ -1,1 +1,15 @@
-<h1>Drag and drop</h1>
+<script>
+    import Dropzone from "svelte-file-dropzone";
+
+
+</script>
+
+<style>
+    :global(custom-dropzone) {
+    }
+</style>
+
+<Dropzone>
+    <h2 class="">Upload your logo</h2>
+    <small>.png o .jpg 1Mo Maximum</small>
+</Dropzone>
