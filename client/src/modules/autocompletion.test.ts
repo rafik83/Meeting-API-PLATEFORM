16b2409d1ad6bc9c95f3c26c40a3696eb6500dcf
Antoine Smagghe <smagghe@proximumgroup.com>
2021-02-24 11:38:02 +0100

    feat 2330 - Remove autocompetion limit

--- client/src/modules/autocompletion.test.ts
+++ client/src/modules/autocompletion.test.ts
@@ -13,19 +13,7 @@ describe('autocompletion', () => {
     ];
     it('should return an array who matched tree first letter for an input string', () => {
       const inputSearch = 'Roy';
-
       const filterCountries = [{ id: 'en', name: 'Royaume-uni' }];
-
-      expect(filterCountriesByName(countries, inputSearch)).toStrictEqual(
-        filterCountries
-      );
-    });
-
-    it('should return undefined if less of 3 characters are input', () => {
-      const inputSearch = 'Ro';
-
-      const filterCountries = undefined;
-
       expect(filterCountriesByName(countries, inputSearch)).toStrictEqual(
         filterCountries
       );
@@ -33,7 +21,6 @@ describe('autocompletion', () => {
 
     it('should return an empty array if the search is not find', () => {
       const inputSearch = 'Fffe';
-
       const filterCountries = [];
       expect(filterCountriesByName(countries, inputSearch)).toStrictEqual(
         filterCountries
@@ -42,7 +29,6 @@ describe('autocompletion', () => {
 
     it('should return a list of results', () => {
       const inputSearch = 'Mon';
-
       const filterCountries = [
         { id: 'mc', name: 'Monaco' },
         { id: 'mn', name: 'Mongolia' },
@@ -55,7 +41,6 @@ describe('autocompletion', () => {
 
     it('should return a list of results -- with ignore case', () => {
       const inputSearch = 'mOn';
-
       const filterCountries = [
         { id: 'mc', name: 'Monaco' },
         { id: 'mn', name: 'Mongolia' },
--- client/src/modules/autocompletion.ts
+++ client/src/modules/autocompletion.ts
@@ -4,9 +4,5 @@ export const filterCountriesByName = (
   datas: Array<Country>,
   search: String
 ): Array<Country> => {
-  if (search.length >= 3) {
-    return datas.filter((data) =>
-      data.name.match(new RegExp('^' + search, 'i'))
-    );
-  }
+  return datas.filter((data) => data.name.match(new RegExp('^' + search, 'i')));
 };
