c74d219280d061ca45b4d6883737595168d48dc9
Volubyl <volubyl@pm.me>
2021-03-11 15:43:29 +0100

    HOTFIX update *.stories.ts to *.stories.js (#70)

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,7 +1,4 @@
 module.exports = {
-  stories: [
-    '../src/components/*.stories.mdx',
-    '../src/components/*.stories.@(js|jsx|ts|tsx)',
-  ],
+  stories: [, '../src/components/*.stories.@(js)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 };
